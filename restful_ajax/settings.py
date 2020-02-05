44fd26463a71d2c717ecb9bf186ad3395d8c6b36
Divinity360 <thedivinitysoft@gmail.com>
2020-02-05 17:04:47 +0100

    new

--- restful_ajax/settings.py
+++ restful_ajax/settings.py
@@ -111,8 +111,7 @@ BASE_URL = 'http://localhost:8000'
 
 LOGIN_REDIRECT_URL = '/api/doctors/add'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
