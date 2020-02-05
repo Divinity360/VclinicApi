7c551ba2479908699c27171d38efb43b31db1430
Divinity360 <thedivinitysoft@gmail.com>
2020-02-05 17:12:20 +0100

    new

--- restful_ajax/settings.py
+++ restful_ajax/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = '8k9-*n^m_kgfaxt0u@@recgf9moyi599d2gqh!81nnng(jijut'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
